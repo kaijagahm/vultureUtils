b42e38fc3dd3edecb28bdfdd7bc4f010f62ddaef
Kaija <kaija.gahm@gmail.com>
2022-09-28 17:55:00 -0700

    updates to wrapper functions (roosting is still not done)

--- R/wrappers.R
+++ R/wrappers.R
@@ -34,11 +34,11 @@ cleanData <- function(dataset, mask, inMaskThreshold = 0.33, crs = "WGS84", long
                                                    thresh = inMaskThreshold,
                                                    dateCol = dateCol)
     dataset <- dataset %>% # using datDF because we don't want to actually restrict it to the mask yet
-      filter(trackId %in% longEnoughIndivs)
+      dplyr::filter(.data[[trackId]] %in% longEnoughIndivs)
   }
 
   # Mask again to remove out-of-mask points.
-  cleanedInMask <- vultureUtils::maskData(dataset = filteredData, mask = mask,
+  cleanedInMask <- vultureUtils::maskData(dataset = dataset, mask = mask,
                                           longCol = longCol,
                                           latCol = latCol,
                                           crs = crs)
@@ -49,60 +49,29 @@ cleanData <- function(dataset, mask, inMaskThreshold = 0.33, crs = "WGS84", long
 #' Create co-feeding edge list
 #'
 #' Given a dataset of GPS points, a geographic mask, and some roost polygons, create an edge list.
-#' @param dataset The GPS dataset to be used to create the edge list.
-#' @param mask The object to use to mask the data. Passed to `vultureUtils::maskData()`. Must be an sf object.
-#' @param roostPolygons Roost polygons. Must be an sf object with a CRS that matches the dataset CRS.
+#' @param cleanedInMask The cleaned GPS dataset to be used to create the edge list. This should be the output from `vultureUtils::cleanData()`.
+#' @param roostPolygons Roost polygons. Must be an sf object with a CRS that matches the cleanedInMask CRS.
 #' @param roostBuffer Number of meters to buffer the roost polygons by before intersecting them. Default is 50 m.
-#' @param inMaskThreshold Proportion of an individual's days tracked that must fall within the mask. Default is 0.33 (one third of days tracked). Passed to `vultureUtils::mostlyInMask()`. Must be numeric.
 #' @param consecThreshold Minimal number of co-occurrences for considering a viable pair of interacting vultures (default is 2 consecutive time steps). Passed to `vultureUtils::spaceTimeGroups()`. Must be numeric.
 #' @param distThreshold The maximum distance (in meters) at which vultures are considered interacting. Default is 50 for co-feeding. Passed to `vultureUtils::spaceTimeGroups()`. Must be numeric.
 #' @param speedThreshUpper Upper speed threshold, in m/s. For co-feeding, default is 5 m/s. Passed to `vultureUtils::filterLocs()`. Must be numeric.
 #' @param speedThreshLower Lower speed threshold, in m/s. For co-feeding, default is NULL. Passed to `vultureUtils::filterLocs()`. Must be numeric.
-#' @param crs Coordinate Reference System to check for and transform to, for both the GPS data and the mask. Default is "WGS84". This value is passed to `vultureUtils::maskData()`. Must be a valid CRS or character string coercible to CRS.
-#' @param longCol The name of the column in the dataset containing longitude values. Defaults to "location_long.1". Passed to `vultureUtils::maskData()`.
-#' @param latCol The name of the column in the dataset containing latitude values. Defaults to "location_lat.1". Passed to `vultureUtils::maskData()`.
-#' @param dateCol The name of the column in the dataset containing dates. Defaults to "dateOnly". Passed to `vultureUtils::mostlyInMask()`.
 #' @return An edge list containing the following columns: `timegroup` gives the numeric index of the timegroup during which the interaction takes place. `minTimestamp` and `maxTimestamp` give the beginning and end times of that timegroup. `ID1` is the trackID of the first individual in this edge, and `ID2` is the trackID of the second individual in this edge.
 #' @export
-getFeedingEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMaskThreshold = 0.33, consecThreshold = 2, distThreshold = 50, speedThreshUpper = 5, speedThreshLower = NULL, crs = "WGS84", longCol = "location_long.1", latCol = "location_lat.1", dateCol = "dateOnly"){
+getFeedingEdges <- function(cleanedInMask, roostPolygons, roostBuffer = 50, consecThreshold = 2, distThreshold = 50, speedThreshUpper = 5, speedThreshLower = NULL, dateCol = "dateOnly"){
   # Argument checks
-  checkmate::assertClass(mask, "sf")
-  checkmate::assertDataFrame(dataset)
+  checkmate::assertDataFrame(cleanedInMask)
   checkmate::assertClass(roostPolygons, "sf")
   checkmate::assertNumeric(roostBuffer, len = 1)
-  checkmate::assertNumeric(inMaskThreshold, len = 1, lower = 0, upper = 1, null.ok = TRUE)
   checkmate::assertNumeric(consecThreshold, len = 1)
   checkmate::assertNumeric(distThreshold, len = 1)
   checkmate::assertNumeric(speedThreshUpper, len = 1, null.ok = TRUE)
   checkmate::assertNumeric(speedThreshLower, len = 1, null.ok = TRUE)
-  checkmate::assertCharacter(longCol, len = 1)
-  checkmate::assertCharacter(latCol, len = 1)
-  checkmate::assertCharacter(dateCol, len = 1)
-  checkmate::assertSubset(x = c(longCol, latCol, dateCol), choices = names(dataset))
-
-  # If an inMaskThreshold is given (it usually is), then filter to only the individuals that spend at least the threshold proportion of their days within the mask. Otherwise, just pass the dataset through unfiltered.
-  if(!is.null(inMaskThreshold)){
-    # Select only points that fall in the mask
-    inMask <- vultureUtils::maskData(dataset = dataset, mask = mask, longCol = longCol,
-                                     latCol = latCol, crs = crs)
-
-    # Remove vultures that have less than `inMaskThreshold` of their duration recorded inside the mask.
-    longEnoughIndivs <- vultureUtils::mostlyInMask(dataset = dataset, maskedDataset = inMask,
-                                                   thresh = inMaskThreshold, dateCol = dateCol)
-    dataset <- dataset %>% # using datDF because we don't want to actually restrict it to the mask yet
-      filter(trackId %in% longEnoughIndivs)
-  }
 
   # Restrict to non-flight interactions.
-  filteredData <- vultureUtils::filterLocs(df = dataset,
+  filteredData <- vultureUtils::filterLocs(df = cleanedInMask,
                                            speedThreshUpper = speedThreshUpper,
                                            speedThreshLower = speedThreshLower)
-
-  # Mask again to remove out-of-mask points.
-  cleanedInMask <- vultureUtils::maskData(dataset = filteredData, mask = mask,
-                                          longCol = longCol,
-                                          latCol = latCol,
-                                          crs = crs)
 
   # Buffer the roost polygons
   roostPolygons <- convertAndBuffer(roostPolygons, dist = roostBuffer)
@@ -111,7 +80,7 @@ getFeedingEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMa
   feedingPoints <- cleanedInMask[lengths(sf::st_intersects(cleanedInMask, roostPolygons)) == 0,]
 
   # Create edge list using spaceTimeGroups
-  feedingEdges <- vultureUtils::spaceTimeGroups(dataset = feedingPoints, distThreshold = distThreshold,
+  feedingEdges <- vultureUtils::spaceTimeGroups(cleanedInMask = feedingPoints, distThreshold = distThreshold,
                                                 consecThreshold = consecThreshold)
 
   # Return the edge list
@@ -121,60 +90,30 @@ getFeedingEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMa
 #' Create co-flight edge list
 #'
 #' Given a dataset of GPS points, a geographic mask, and some roost polygons, create an edge list.
-#' @param dataset The GPS dataset to be used to create the edge list.
+#' @param cleanedInMask The cleaned GPS dataset to be used to create the edge list. This should be the output from `vultureUtils::cleanData()`.
 #' @param mask The object to use to mask the data. Passed to `vultureUtils::maskData()`. Must be an sf object.
 #' @param roostPolygons Roost polygons. Must be an sf object with a CRS that matches the dataset CRS.
 #' @param roostBuffer Number of meters to buffer the roost polygons by before intersecting them. Default is 50 m.
-#' @param inMaskThreshold Proportion of an individual's days tracked that must fall within the mask. Default is 0.33 (one third of days tracked). Passed to `vultureUtils::mostlyInMask()`. Must be numeric.
 #' @param consecThreshold Minimal number of co-occurrences for considering a viable pair of interacting vultures (default is 2 consecutive time steps). Passed to `vultureUtils::spaceTimeGroups()`. Must be numeric.
 #' @param distThreshold The maximum distance (in meters) at which vultures are considered interacting. Default is 1000 m for co-flight Passed to `vultureUtils::spaceTimeGroups()`. Must be numeric.
 #' @param speedThreshUpper Upper speed threshold, in m/s. For co-flight, default is NULL. Passed to `vultureUtils::filterLocs()`. Must be numeric.
 #' @param speedThreshLower Lower speed threshold, in m/s. For co-flight, default is 5 m/s. Passed to `vultureUtils::filterLocs()`. Must be numeric.
-#' @param crs Coordinate Reference System to check for and transform to, for both the GPS data and the mask. Default is "WGS84". This value is passed to `vultureUtils::maskData()`. Must be a valid CRS or character string coercible to CRS.
-#' @param longCol The name of the column in the dataset containing longitude values. Defaults to "location_long.1". Passed to `vultureUtils::maskData()`.
-#' @param latCol The name of the column in the dataset containing latitude values. Defaults to "location_lat.1". Passed to `vultureUtils::maskData()`.
-#' @param dateCol The name of the column in the dataset containing dates. Defaults to "dateOnly". Passed to `vultureUtils::mostlyInMask()`.
 #' @return An edge list containing the following columns: `timegroup` gives the numeric index of the timegroup during which the interaction takes place. `minTimestamp` and `maxTimestamp` give the beginning and end times of that timegroup. `ID1` is the trackID of the first individual in this edge, and `ID2` is the trackID of the second individual in this edge.
 #' @export
-getFlightEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMaskThreshold = 0.33, consecThreshold = 2, distThreshold = 1000, speedThreshUpper = NULL, speedThreshLower = 5, crs = "WGS84", longCol = "location_long.1", latCol = "location_lat.1", dateCol = "dateOnly"){
+getFlightEdges <- function(cleanedInMask, roostPolygons, roostBuffer = 50, consecThreshold = 2, distThreshold = 1000, speedThreshUpper = NULL, speedThreshLower = 5){
   # Argument checks
-  checkmate::assertClass(mask, "sf")
-  checkmate::assertDataFrame(dataset)
+  checkmate::assertDataFrame(cleanedInMask)
   checkmate::assertClass(roostPolygons, "sf")
   checkmate::assertNumeric(roostBuffer, len = 1)
-  checkmate::assertNumeric(inMaskThreshold, len = 1, lower = 0, upper = 1, null.ok = TRUE)
   checkmate::assertNumeric(consecThreshold, len = 1)
   checkmate::assertNumeric(distThreshold, len = 1)
   checkmate::assertNumeric(speedThreshUpper, len = 1, null.ok = TRUE)
   checkmate::assertNumeric(speedThreshLower, len = 1, null.ok = TRUE)
-  checkmate::assertCharacter(longCol, len = 1)
-  checkmate::assertCharacter(latCol, len = 1)
-  checkmate::assertCharacter(dateCol, len = 1)
-  checkmate::assertSubset(x = c(longCol, latCol, dateCol), choices = names(dataset))
-
-  # If an inMaskThreshold is given (it usually is), then filter to only the individuals that spend at least the threshold proportion of their days within the mask. Otherwise, just pass the dataset through unfiltered.
-  if(!is.null(inMaskThreshold)){
-    # Select only points that fall in the mask
-    inMask <- vultureUtils::maskData(dataset = dataset, mask = mask, longCol = longCol,
-                                     latCol = latCol, crs = crs)
-
-    # Remove vultures that have less than `inMaskThreshold` of their duration recorded inside the mask.
-    longEnoughIndivs <- vultureUtils::mostlyInMask(dataset = dataset, maskedDataset = inMask,
-                                                   thresh = inMaskThreshold, dateCol = dateCol)
-    dataset <- dataset %>% # using datDF because we don't want to actually restrict it to the mask yet
-      filter(trackId %in% longEnoughIndivs)
-  }
 
   # Restrict to flight interactions.
-  filteredData <- vultureUtils::filterLocs(df = dataset,
+  filteredData <- vultureUtils::filterLocs(df = cleanedInMask,
                                            speedThreshUpper = speedThreshUpper,
                                            speedThreshLower = speedThreshLower)
-
-  # Mask again to remove out-of-mask points.
-  cleanedInMask <- vultureUtils::maskData(dataset = filteredData, mask = mask,
-                                          longCol = longCol,
-                                          latCol = latCol,
-                                          crs = crs)
 
   # Buffer the roost polygons
   roostPolygons <- convertAndBuffer(roostPolygons, dist = roostBuffer)
@@ -190,60 +129,30 @@ getFlightEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMas
   return(flightEdges)
 }
 
-#' Create co-roosting edge list
+#' Create co-roosting edge list XXX START HERE
 #'
 #' Given a dataset of GPS points, a geographic mask, and some roost polygons, create an edge list.
-#' @param dataset The GPS dataset to be used to create the edge list.
-#' @param mask The object to use to mask the data. Passed to `vultureUtils::maskData()`. Must be an sf object.
+#' @param cleanedInMask The cleaned GPS dataset to be used to create the edge list. This should be the output from `vultureUtils::cleanData()`.
 #' @param roostPolygons Roost polygons. Must be an sf object with a CRS that matches the dataset CRS.
 #' @param roostBuffer Number of meters to buffer the roost polygons by before intersecting them. Default is 50 m.
-#' @param inMaskThreshold Proportion of an individual's days tracked that must fall within the mask. Default is 0.33 (one third of days tracked). Passed to `vultureUtils::mostlyInMask()`. Must be numeric.
 #' @param consecThreshold Minimal number of co-occurrences for considering a viable pair of interacting vultures (default is 2 consecutive time steps). Passed to `vultureUtils::spaceTimeGroups()`. Must be numeric.
-#' @param crs Coordinate Reference System to check for and transform to, for both the GPS data and the mask. Default is "WGS84". This value is passed to `vultureUtils::maskData()`. Must be a valid CRS or character string coercible to CRS.
-#' @param longCol The name of the column in the dataset containing longitude values. Defaults to "location_long.1". Passed to `vultureUtils::maskData()`.
-#' @param latCol The name of the column in the dataset containing latitude values. Defaults to "location_lat.1". Passed to `vultureUtils::maskData()`.
-#' @param dateCol The name of the column in the dataset containing dates. Defaults to "dateOnly". Passed to `vultureUtils::mostlyInMask()`.
 #' @return An edge list containing the following columns: `timegroup` gives the numeric index of the timegroup during which the interaction takes place. `minTimestamp` and `maxTimestamp` give the beginning and end times of that timegroup. `ID1` is the trackID of the first individual in this edge, and `ID2` is the trackID of the second individual in this edge.
 #' @export
-getRoostingEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inMaskThreshold = 0.33, consecThreshold = 2, crs = "WGS84", longCol = "location_long.1", latCol = "location_lat.1", dateCol = "dateOnly"){
+getRoostingEdges <- function(cleanedInMask, roostPolygons, roostBuffer = 50,
+                             consecThreshold = 2){
   # Argument checks
-  checkmate::assertClass(mask, "sf")
-  checkmate::assertDataFrame(dataset)
+  checkmate::assertDataFrame(cleanedInMask)
   checkmate::assertClass(roostPolygons, "sf")
   checkmate::assertNumeric(roostBuffer, len = 1)
-  checkmate::assertNumeric(inMaskThreshold, len = 1, lower = 0, upper = 1, null.ok = TRUE)
   checkmate::assertNumeric(consecThreshold, len = 1)
   checkmate::assertNumeric(distThreshold, len = 1)
   checkmate::assertNumeric(speedThreshUpper, len = 1, null.ok = TRUE)
   checkmate::assertNumeric(speedThreshLower, len = 1, null.ok = TRUE)
-  checkmate::assertCharacter(longCol, len = 1)
-  checkmate::assertCharacter(latCol, len = 1)
-  checkmate::assertCharacter(dateCol, len = 1)
-  checkmate::assertSubset(x = c(longCol, latCol, dateCol), choices = names(dataset))
-
-  # If an inMaskThreshold is given (it usually is), then filter to only the individuals that spend at least the threshold proportion of their days within the mask. Otherwise, just pass the dataset through unfiltered.
-  if(!is.null(inMaskThreshold)){
-    # Select only points that fall in the mask
-    inMask <- vultureUtils::maskData(dataset = dataset, mask = mask, longCol = longCol,
-                                     latCol = latCol, crs = crs)
-
-    # Remove vultures that have less than `inMaskThreshold` of their duration recorded inside the mask.
-    longEnoughIndivs <- vultureUtils::mostlyInMask(dataset = dataset, maskedDataset = inMask,
-                                                   thresh = inMaskThreshold, dateCol = dateCol)
-    dataset <- dataset %>% # using datDF because we don't want to actually restrict it to the mask yet
-      filter(trackId %in% longEnoughIndivs)
-  }
 
   # Restrict to flight interactions.
   filteredData <- vultureUtils::filterLocs(df = dataset,
                                            speedThreshUpper = speedThreshUpper,
                                            speedThreshLower = speedThreshLower)
-
-  # Mask again to remove out-of-mask points.
-  cleanedInMask <- vultureUtils::maskData(dataset = filteredData, mask = mask,
-                                          longCol = longCol,
-                                          latCol = latCol,
-                                          crs = crs)
 
   # Buffer the roost polygons
   roostPolygons <- convertAndBuffer(roostPolygons, dist = roostBuffer)
@@ -271,5 +180,3 @@ getRoostingEdges <- function(dataset, mask, roostPolygons, roostBuffer = 50, inM
 # iv. If owing to the 50m buffer around roost polygons, a location is assigned to >1 roost, then remove duplicates and
 # -----> assign the location to the closer roost polygon
 #
-
-
