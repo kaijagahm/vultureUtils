2f30eee897aec91e9421d5f14c74f5bc03bfba67
Kaija <kaija.gahm@gmail.com>
2022-09-29 18:16:20 -0700

    Expanded capabilities of removeUnnecessaryVars

--- R/funs.R
+++ R/funs.R
@@ -64,25 +64,36 @@ downloadVultures <- function(loginObject, extraSensors = F, removeDup = T,
 #'
 #' Remove variables we don't need, so the data is smaller
 #' @param dataset a dataset to remove variables from. Must be a data frame.
+#' @param addlVars a character vector of additional variables to remove
+#' @param keepVars a character vector of variables to keep that would otherwise be removed.
 #' @return A dataset, with variables removed
 #' @export
-removeUnnecessaryVars <- function(dataset){
+removeUnnecessaryVars <- function(dataset, addlVars = NULL, keepVars = NULL){
   checkmate::assertDataFrame(dataset) # must be a data frame
+  checkmate::assertCharacter(addlVars, null.ok = TRUE)
+  checkmate::assertCharacter(keepVars, null.ok = TRUE)
+
+  # combine `addlVars` with the original list of vars to remove
+  toRemove <- c(addlVars, "sensor_type_id","taxon_canonical_name","nick_name","earliest_date_born","sensor","optional",
+                "sensor_type","mw_activity_count","eobs_accelerations_raw","eobs_acceleration_sampling_frequency_per_axis",
+                "eobs_acceleration_axes","argos_valid_location_algorithm","argos_sensor_4","argos_sensor_3","argos_sensor_2",
+                "argos_sensor_1","argos_semi_minor","argos_semi_major","argos_pass_duration","argos_orientation","argos_nopc",
+                "argos_lat1","argos_lat2","1084088","argos_lon1","argos_lon2","argos_nb_mes","argos_nb_mes_120",
+                "eobs_key_bin_checksum","eobs_fix_battery_voltage","eobs_battery_voltage","eobs_status",
+                "eobs_start_timestamp","eobs_type_of_fix","eobs_used_time_to_get_fix","eobs_temperature",
+                "gps_dop","magnetic_field_raw_x","magnetic_field_raw_y","magnetic_field_raw_z","ornitela_transmission_protocol",
+                "tag_voltage","algorithm_marked_outlier","argos_altitude","argos_best_level","argos_lc","argos_iq",
+                "argos_gdop","argos_error_radius","argos_calcul_freq","timestamps","height_raw",
+                "barometric_pressure","barometric_height","battery_charging_current","eobs_activity","manually_marked_outlier",
+                "eobs_activity_samples", "acceleration_raw_y", "battery_charge_percent", "data_decoding_software","gps_vdop","height_above_ellipsoid",
+                'acceleration_raw_x','acceleration_raw_z',"acceleration_raw_z","eobs_horizontal_accuracy_estimate","eobs_speed_accuracy_estimate")
+
+  # get rid of any vars we'd like to keep
+  toRemove <- toRemove[!(toRemove %in% keepVars)]
+
+  # actually do the removal
   newDataset <- dataset %>%
-    dplyr::select(-dplyr::any_of(c("sensor_type_id","taxon_canonical_name","nick_name","earliest_date_born","sensor","optional",
-                            "sensor_type","mw_activity_count","eobs_accelerations_raw","eobs_acceleration_sampling_frequency_per_axis",
-                            "eobs_acceleration_axes","argos_valid_location_algorithm","argos_sensor_4","argos_sensor_3","argos_sensor_2",
-                            "argos_sensor_1","argos_semi_minor","argos_semi_major","argos_pass_duration","argos_orientation","argos_nopc",
-                            "argos_lat1","argos_lat2","1084088","argos_lon1","argos_lon2","argos_nb_mes","argos_nb_mes_120",
-                            "eobs_key_bin_checksum","eobs_fix_battery_voltage","eobs_battery_voltage","eobs_status",
-                            "eobs_start_timestamp","eobs_type_of_fix","eobs_used_time_to_get_fix","eobs_temperature",
-                            "gps_dop","magnetic_field_raw_x","magnetic_field_raw_y","magnetic_field_raw_z","ornitela_transmission_protocol",
-                            "tag_voltage","algorithm_marked_outlier","argos_altitude","argos_best_level","argos_lc","argos_iq",
-                            "argos_gdop","argos_error_radius","argos_calcul_freq","timestamps","height_raw",
-                            "barometric_pressure","barometric_height","battery_charging_current","eobs_activity","manually_marked_outlier",
-                            "eobs_activity_samples", "acceleration_raw_y", "battery_charge_percent", "data_decoding_software","gps_vdop","height_above_ellipsoid",
-                            'acceleration_raw_x','acceleration_raw_z',"acceleration_raw_z","eobs_horizontal_accuracy_estimate","eobs_speed_accuracy_estimate")
-    ))
+    dplyr::select(-dplyr::any_of(toRemove))
   return(newDataset)
 }
 
