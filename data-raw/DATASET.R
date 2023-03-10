ae31f0586d4a501d69fec4254b26d4f7a4594776
Kaija <kaija.gahm@gmail.com>
2023-03-10 13:42:16 -0800

    referencing external dir for test data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,4 +1,14 @@
-## code to prepare `DATASET` dataset goes here
+## This code prepares datasets from Movebank for testing.
 
+## 3-day datasets
+base::load("~/Desktop/otherDir/movebankCredentials/pw.Rda")
+MB.LoginObject <- move::movebankLogin(username = 'kaijagahm', password = pw)
+rm(pw)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# Download two 3-day datasets from movebank (note: will have to separately ensure that downloadVultures is functioning as expected.)
+jan01_03_2021_raw <- downloadVultures(loginObject = MB.LoginObject, dateTimeStartUTC = "2021-01-01 00:00", dateTimeEndUTC = "2021-03-01 11:59")
+jun01_03_2022_raw <- downloadVultures(loginObject = MB.LoginObject, dateTimeStartUTC = "2022-06-01 00:00", dateTimeEndUTC = "2022-06-03 11:59")
+
+# Save these as internal datasets
+save(jan01_03_2021_raw, file = "~/Desktop/otherDir/jan01_03_2021_raw.Rda")
+save(jun01_03_2022_raw, file = "~/Desktop/otherDir/jun01_03_2022_raw.Rda")
