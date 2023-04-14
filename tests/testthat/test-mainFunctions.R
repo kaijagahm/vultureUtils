1441729f29e9ec2c8544030efa3bee2fae290c65
Kaija <kaija.gahm@gmail.com>
2023-04-13 22:14:27 -0700

    some tests for get_roosts_df

--- tests/testthat/test-mainFunctions.R
+++ tests/testthat/test-mainFunctions.R
@@ -14,3 +14,26 @@ test_that("getFeedingEdges works", {
   expect_error(getFeedingEdges(dataset = a, roostPolygons = r, idCol = "id", return = "edges"))
 })
 
+test_that("get_roosts_df works", {
+  base::load(test_path("testdata", "a.Rda"))
+  aSF <- sf::st_as_sf(a, coords = c("location_long", "location_lat"), remove = F, crs = "WGS84") %>%
+    dplyr::mutate(timestamp = lubridate::ymd_hms(timestamp),
+                  dateOnly = lubridate::ymd(dateOnly))
+
+  # A normal run
+  ## messages
+  m <- capture_output(get_roosts_df(aSF, id = "id"))
+  expect_match(m, "Finding roosts... this may take a while if your dataset is large.", all = FALSE)
+  expect_match(m, "Roost computation completed in", all = FALSE)
+
+  ## create the output
+  roostsOut <- get_roosts_df(aSF, id = "id", quiet = T)
+
+  ## test the output
+  expect_equal(class(roostsOut), c("tbl_df", "tbl", "data.frame")) # output is a tibble/data frame
+  expect_equal(names(roostsOut), c("id", "date", "roost_date", "sunrise", "sunset", "sunrise_twilight", "sunset_twilight", "daylight", "is_roost", "location_lat", "location_long"
+  ))
+  expect_equal(unique(roostsOut$id), unique(aSF$id))
+  expect_equal(all(roostsOut$date %in% aSF$dateOnly), TRUE)
+  expect_equal(nrow(roostsOut %>% dplyr::distinct(id, date)) < nrow(aSF %>% dplyr::distinct(id, dateOnly)), TRUE)
+})
