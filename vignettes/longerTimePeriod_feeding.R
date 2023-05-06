a71c9cad02e668a37564dec44cefb3fd971bf02b
Kaija <kaija.gahm@gmail.com>
2023-05-05 18:05:00 -0700

    longer time period

--- vignettes/longerTimePeriod_feeding.R
+++ vignettes/longerTimePeriod_feeding.R
@@ -3,7 +3,7 @@ load("../../Downloads/mydata_1.rda")
 # Make a single column containing just one grouping variable for the entire time period. Here is an example grouping by weeks:
 # Now feed it into getFeedingEdges
 edges <- getFeedingEdges(dataset = mydata_1, roostPolygons = NULL,
-                         roostBuffer = 50, consecThreshold = 1, distThreshold = 25, speedThreshUpper = 5, speedThreshLower = NULL, timeThreshold = "1 day", idCol = "id", return = "edges")
+                         roostBuffer = 50, consecThreshold = 2, distThreshold = 25, speedThreshUpper = 5, speedThreshLower = NULL, timeThreshold = "20 days", idCol = "id", return = "edges")
 
 # Now we can use some post-processing to look at how many times each dyad interacts.
 # Make sure we only have edges going one way, and no self edges:
