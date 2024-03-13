461f9f0c12a5553e02df3645b04980af31dbc6d1
Kaija <kaija.gahm@gmail.com>
2024-03-13 12:53:42 -0700

    more descriptive comment

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,3 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `sysdata.Rda` dataset goes here
 jamPolygons <- sf::st_read("data-raw/GPS_jamming_3.kml")
 usethis::use_data(jamPolygons, internal = TRUE, overwrite = TRUE)
